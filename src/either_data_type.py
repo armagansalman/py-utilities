ad7c401035ea595797bd7dcd58d4f66143313784
Armagan Salman <armagansalman,gmail>
2022-04-07 13:55:20 +0300

    Cosmetic changes.

--- src/either_data_type.py
+++ src/either_data_type.py
@@ -1,11 +1,24 @@
 """
 Copyright © 2022 Armağan Salman
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+Permission is hereby granted, free of charge, to any person 
+obtaining a copy of this software and associated documentation 
+files (the “Software”), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, 
+publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be 
+included in all copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, 
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF 
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY 
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE 
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
 
@@ -87,17 +100,18 @@ class Either:
 ### End: class Either
 
 
-eit_l = Either.left("left value")
-eit_r = Either.right("right value")
+if __name__ == "__main__":
+    eit_l = Either.left("left value")
+    eit_r = Either.right("right value")
 
-print(eit_l.is_left())
-print(eit_l.get_left_or(555))
-print(eit_r.get_left_or(555))
+    print(eit_l.is_left())
+    print(eit_l.get_left_or(555))
+    print(eit_r.get_left_or(555))
 
-print(eit_r.is_left())
-print(eit_r.is_right())
-print(eit_r.get_left_or(777))
-print(eit_l.get_left_or(777))
+    print(eit_r.is_left())
+    print(eit_r.is_right())
+    print(eit_r.get_left_or(777))
+    print(eit_l.get_left_or(777))
 
-print(eit_l)
-
+    print(eit_l)
+#
