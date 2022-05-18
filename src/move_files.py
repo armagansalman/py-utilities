428ab5196ef3e11fc58ee85bef6473865f4334e3
Armağan Salman <4987651+armagansalman@users.noreply.github.com>
2022-05-18 15:14:12 +0300

    move_files can create strpacks from all files from a directory.

--- src/move_files.py
+++ src/move_files.py
@@ -24,13 +24,32 @@ SOFTWARE.
 
 """
 
+
+"""
+        WHAT this module is for
+    + This module was meant to provide functions that'll move many files in many
+    dirs into one dir while adding id's to files.
+    + The paths files have before copying will be stored in a .txt file along with
+    the date of copy.
+"""
+
+
 #( TODO(armagan)-2022-05-17T22:25
-#  For each file F under directory D(F), create a new dir named "file store" under D(F) and put
-#  all F's inside it. Give them unique ids. Store id and name relations inside a txt file under D(F)/file store so that original file names can be restored if needed. Also put original paths of the files inside that txt file.
+#( Update(armagan)-2022-05-18T13:01:15
+#  Do things below only for relational file manager. Don't expand this module beyond
+#  its responsibilities.
+#)
+#  For each file F under directory D(F), create a new dir named "file store" under
+#  D(F) and put all F's inside it. Give them unique ids. Store id and name 
+#  relations inside a txt file under D(F)/file store so that original file names
+#  can be restored if needed. Also put original paths of the files inside that txt
+#  file.
 #)
 
+
 import os
-from pathlib import PurePath
+from pathlib import Path, PurePath
+import datetime
 
 
 from type_aliases import *
@@ -39,6 +58,23 @@ from string_transformer import t_StrData, t_StrPack, t_StrPackList, t_PackCreato
 
 import text_file_io as TxtIO
 
+
+
+def get_utc_datetime_now():
+#(
+    return datetime.datetime.now()
+#)
+
+
+def time_now_iso8601():
+#(
+    now = get_utc_datetime_now()
+    
+    now_str = "{}-{}-{}T{}:{}:{}".format(now.year, now.month, \
+        now.day, now.hour, now.minute, now.second)
+    
+    return now_str
+#)
 
 
 def get_fpaths_recursively(PATH: str):
@@ -112,14 +148,90 @@ def try_2():
 #)
 
 
+def try_3():
+#(
+    dir_1 = "D:\\ALL BOOKS-PAPERS"
+    
+    all_files = get_fpaths_recursively(dir_1)
+    
+    lined_paths = '\n'.join(all_files)
+    
+    out_file = "file_list.txt"
+    
+    TxtIO.append_utf8_list(out_file, lined_paths)
+#)
+
+
+def try_4():
+#(
+    dir_1 = "D:\\ALL BOOKS-PAPERS"
+    
+    files_abs = get_fpaths_recursively(dir_1)
+    
+    strpacks = []
+    for path in files_abs:
+    #( Split filenames from their dirs. Collect them as packs.
+        path_obj = Path(path)
+        
+        fname = path_obj.name
+        
+        pack = ( fname, {"source_full_path": path_obj.as_posix()} )
+        
+        strpacks.append(pack)
+    #)
+    
+    out_data = map(str, strpacks)
+    out_data = "\n\n".join(out_data)
+    
+    out_file = "try_4-file_list.txt"
+    
+    TxtIO.remove_file(out_file)
+    TxtIO.append_utf8_list(out_file, out_data)
+#)
+
+
+def try_5():
+#(
+    dir_1 = "D:\\ALL BOOKS-PAPERS"
+    
+    files_abs = get_fpaths_recursively(dir_1)
+    
+    strpacks = []
+    for path in files_abs:
+    #( Split filenames from their dirs. Collect them as packs.
+        path_obj = Path(path)
+        
+        fname = path_obj.name
+        
+        pack = ( fname, {"source_full_path": path_obj.as_posix()} )
+        
+        strpacks.append(pack)
+    #)
+    
+    out_data = map(str, strpacks)
+    out_data = "\n\n".join(out_data)
+    
+    out_file = "try_4-file_list.txt"
+    
+    TxtIO.remove_file(out_file)
+    TxtIO.append_utf8_list(out_file, out_data)
+#)
+
+
 if __name__ == "__main__":
 #(
     # try_1() # Get all full paths.
     
-    fname, fdirparts = try_2()
-    new_fname = "r10_1--" + fname
-    print(new_fname)
-    print(fdirparts)
+    #fname, fdirparts = try_2()
+    #new_fname = "r10_1--" + fname
+    #print(new_fname)
+    #print(fdirparts)
+    
+    # try_3()
+    
+    # try_4()
+    
+    assert(1 == 0)
 #)
 
 
@@ -130,6 +242,17 @@ To do overwrite, one must first call remove_file function then call append_utf8_
 text_file_io.py
     remove_file(fileref: t_Str)
     append_utf8_list(fileref: t_Str, data: t_List[t_Str])
+"""
+
+
+"""
+#(
+import os
+def fixpath(path):
+    return os.path.abspath(os.path.expanduser(path))
+#)
+os.path.basename(file_path)  # Returns file name from path string.
+os.path.dirname(real_path)  # Returns directory name from path string.
 """
 
 
@@ -147,4 +270,41 @@ https://stackoverflow.com/a/26329375
 >>> p = p.relative_to('/var/tmp/workdir')
 >>> p.parts
 ('1', 'foo', 'bar', 'test.jpg')
+"""
+
+
+
+
+"""
+def try_4():
+#(
+    dir_1 = "D:\\ALL BOOKS-PAPERS"
+    
+    files_abs = get_fpaths_recursively(dir_1)
+    
+    strpacks = []
+    for path in files_abs:
+    #( Split filenames from their dirs. Collect them as packs.
+        # fname = os.path.basename(path)
+        # file_dir = os.path.dirname(path)
+        
+        p = PurePath(path)
+        
+        fname = p.parts[-1]  # Last part is filename.
+        dirparts = tuple( p.parts[ : -1] )  # Every part till the last is a dir part.
+        
+        pack = ( fname, {"dirparts": dirparts} )
+        
+        strpacks.append(pack)
+    #)
+    
+    
+    out_data = map(str, strpacks)
+    out_data = '\n'.join(out_data)
+    
+    out_file = "try_4-file_list.txt"
+    
+    TxtIO.remove_file(out_file)
+    TxtIO.append_utf8_list(out_file, out_data)
+#)
 """
