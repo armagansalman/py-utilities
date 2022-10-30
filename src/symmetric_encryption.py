adb7822dd15fb22f1b66b9843a5da3db0d953e60
Armagan Salman <4987651+armagansalman@users.noreply.github.com>
2022-10-30 22:01:07 +0300

    Changed License to BSD-3 AND Started symmetric_encryption.py

--- src/symmetric_encryption.py
+++ src/symmetric_encryption.py
@@ -1,3 +1,5 @@
+"""
+
 BSD 3-Clause License
 
 Copyright (c) 2022, Armağan Salman
@@ -27,3 +29,44 @@ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
 CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+#
+""" pip install cryptography
+https://cryptography.io/en/latest/
+"""
+#
+#
+import base64
+
+import os
+
+from cryptography.fernet import Fernet
+
+from cryptography.hazmat.primitives import hashes
+#
+#
+def tutorial():
+    """doc"""
+#(
+    # Put this (key) somewhere safe!
+    key = Fernet.generate_key()
+    print(f"Fernet key: {key}")
+
+    f = Fernet(key)
+
+    token = f.encrypt(b"A really secret message. Not for prying eyes.")
+
+    print(f"token: {token}")
+    #b'...'
+
+    decrypted = f.decrypt(token)
+    print(f"decrypted: {decrypted}")
+    #b'A really secret message. Not for prying eyes.'
+#)
+#
+#
+def gen_scrypt_key_derivation_func():
+#(
+    os.urandom(salt_bit_width) # Returns bytes with given length.
+#)
